6aa3f8eb1d14bc7b1196a10e1b601ef92f8d80f2
裴智慧 <13269724310>
2017-08-02 16:34:43 +0800

    update

--- spec/controller/users_controller_spec.rb
+++ spec/controller/users_controller_spec.rb
@@ -4,16 +4,16 @@ describe UsersController do
 
   context "signup" do
     it "should failed" do
-      post :create, params: { user: { email: 'eggman' } }
+      post :create, params: { user: { email: 'zhihui' } }
       expect(response).to render_template("new")
     end
 
     it "should success" do
       post :create, params: { user:
-        {
-          email: 'eggman@example.com',
-          password: '111111',
-          password_confirmation: '111111'
+                              {
+        email: '13269724310@163.com',
+        password: '666666',
+        password_confirmation: '666666'
         }
       }
       expect(response).to redirect_to(new_session_path)
