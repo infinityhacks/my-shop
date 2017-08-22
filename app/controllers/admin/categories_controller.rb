965ce4ed28cb5cf41d4507716478d564092b88e5
裴智慧 <13269724310>
2017-08-22 20:08:16 +0800

    二级分类查询

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,17 +1,24 @@
 class Admin::CategoriesController < Admin::BaseController
 
+  before_action :find_root_categories, only: [:new, :create, :edit, :update]
+  before_action :find_category, only: [:edit, :update, :destroy]
+
   def index
-    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
+    if params[:id].blank?
+      @categories = Category.roots
+    else
+      @category = Category.find params[:id]
+      @categories = @category.children
+    end
+    @categories = @categories.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
     @category = Category.new
-    @root_categories = Category.roots.order(id: "desc")
   end
 
   def create
     @category = Category.new(params.require(:category).permit!)
-    @root_categories = Category.roots.order(id: "desc")
 
     if @category.save
       flash[:notice] = "保存成功"
@@ -22,13 +29,10 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def edit
-    @category = Category.find(params[:id])
-    @root_categories = Category.roots.order(id: "desc")
     render action: :new
   end
 
   def update
-    @category = Category.find params[:id]
     @category.attributes = params.require(:category).permit!
     if @category.save
       flash[:notice] = "修改成功"
@@ -36,7 +40,25 @@ class Admin::CategoriesController < Admin::BaseController
     else
       render action: :new
     end
+  end
 
+  def destroy
+    if @category.destroy
+      flash[:notice] = "删除成功"
+      redirect_to admin_categories_path
+    else
+      flash[:notice] = "删除失败"
+      redirect_to :back
+    end
+  end
+
+  private
+  def find_root_categories
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+    @category = Category.find params[:id]
   end
 
 end
