6987614755d916501482bea5aa84c47b5634f227
裴智慧 <13269724310>
2017-08-22 20:14:23 +0800

    分类级联删除

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,9 +1,11 @@
 class Category < ApplicationRecord
 
   validates :title, presence: { message: "名称不能为空"}
+  validates :title, uniqueness: { message: "名称不能重复"}
 
   #因为category模型要用到ancestry，需要配置一下 -> has_ancestry
-  has_ancestry
+  #如果删除了一级分类，二级分类也删除掉。orphan_strategy: :destroy
+  has_ancestry orphan_strategy: :destroy
 
   has_many :products, dependent: :destroy #如果这个某个分类被删除，分类下的所有类也要删除,级联删除
 
